8b7e86ea3bc7538376f2f9510ce6797345374864
tofid <andreyklochok@gmail.com>
2016-11-07 17:48:13 +0200

    Added Part block

--- src/views/dashboard/index.php
+++ src/views/dashboard/index.php
@@ -7,6 +7,8 @@ use hipanel\modules\dashboard\widgets\SmallBox;
 use hipanel\modules\domain\models\DomainSearch;
 use hipanel\modules\finance\models\BillSearch;
 use hipanel\modules\server\models\ServerSearch;
+use hipanel\modules\stock\models\ModelSearch;
+use hipanel\modules\stock\models\PartSearch;
 use hipanel\modules\ticket\models\ThreadSearch;
 use yii\helpers\Html;
 use yii\helpers\Url;
@@ -162,4 +164,32 @@ $this->title = Yii::t('hipanel:dashboard', 'Dashboard');
             <?php $box::end() ?>
         </div>
     <?php endif ?>
+
+    <?php if (Yii::getAlias('@part', false) && Yii::$app->user->can('manage')) : ?>
+        <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+            <?php $box = SmallBox::begin([
+                'boxTitle' => Yii::t('hipanel:stock', 'Parts'),
+                'boxIcon' => 'fa-cubes',
+                'boxColor' => SmallBox::COLOR_YELLOW,
+            ]) ?>
+            <?php $box->beginBody() ?>
+            <br>
+            <br>
+            <?= SearchForm::widget([
+                'formOptions' => [
+                    'id' => 'part-search',
+                    'action' => Url::to('@part/index'),
+                ],
+                'model' => new PartSearch(),
+                'attribute' => 'serial_like',
+                'buttonColor' => SmallBox::COLOR_YELLOW,
+            ]) ?>
+            <?php $box->endBody() ?>
+            <?php $box->beginFooter() ?>
+            <?= Html::a(Yii::t('hipanel', 'View') . $box->icon(), '@part/index', ['class' => 'small-box-footer']) ?>
+            <?php $box->endFooter() ?>
+            <?php $box::end() ?>
+        </div>
+    <?php endif ?>
+
 </div>
