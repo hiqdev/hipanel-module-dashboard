dbb615d2118e8f1714cc07ee4811193b7f306f44
tofid <andreyklochok@gmail.com>
2016-12-02 18:19:44 +0200

    Made View in Domain block is shown

--- src/views/dashboard/index.php
+++ src/views/dashboard/index.php
@@ -48,9 +48,7 @@ $this->title = Yii::t('hipanel:dashboard', 'Dashboard');
             <div class="clearfix"></div>
             <?php $box->endBody() ?>
             <?php $box->beginFooter() ?>
-            <?php if (Yii::$app->user->can('support')) : ?>
-                <?= Html::a(Yii::t('hipanel', 'View') . $box->icon(), '@domain/index', ['class' => 'small-box-footer']) ?>
-            <?php endif ?>
+            <?= Html::a(Yii::t('hipanel', 'View') . $box->icon(), '@domain/index', ['class' => 'small-box-footer']) ?>
             <?php if ($model->count['contacts']) : ?>
                 <?= Html::a(Yii::t('hipanel', 'Contacts') . ': ' . $model->count['contacts'] . $box->icon(), '@contact/index', ['class' => 'small-box-footer']) ?>
             <?php endif ?>
