a284ba75da46eb2f79544189ce03c0d1035164c3
Andrii Vasyliev <sol@hiqdev.com>
2016-12-22 13:34:31 +0000

    redone yii2-thememanager -> yii2-menus

--- src/menus/SidebarMenu.php
+++ src/menus/SidebarMenu.php
@@ -13,7 +13,7 @@ namespace hipanel\modules\dashboard\menus;
 
 use Yii;
 
-class SidebarMenu extends \hiqdev\menumanager\Menu
+class SidebarMenu extends \hiqdev\yii2\menus\Menu
 {
     public function items()
     {
