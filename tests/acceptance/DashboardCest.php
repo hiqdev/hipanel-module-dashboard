c358e3fe7a68be2b9fe2e14110ac981dbd33cac4
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2018-05-02 18:40:36 +0300

    Moved tests into this repo

---
+++ tests/acceptance/DashboardCest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace acceptance\client\tests\dashboard;
+
+use hipanel\tests\_support\Step\Acceptance\Client;
+use yii\helpers\Url;
+
+class DashboardCest
+{
+    public function ensureThatDashboardWorks(Client $I)
+    {
+        $I->login();
+        $I->amOnPage(Url::to(['/']));
+        $I->seeInCurrentUrl('/dashboard/dashboard');
+        $I->see('Dashboard');
+        $I->performOn('.content-wrapper', \Codeception\Util\ActionSequence::build()
+            ->see('Domains')
+            ->see('Servers')
+            ->see('Tickets')
+            ->see('Finance')
+            ->dontSee('Clients')
+            ->dontSee('Tariffs')
+            ->dontSee('Models')
+        );
+    }
+}
