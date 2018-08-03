39cee807965030b2a48140fe5fe6e422b13874f0
pkolomiy <pkolomiy@gmail.com>
2018-08-03 14:58:16 +0300

    Added DashboardCest for admin and seller

--- tests/acceptance/admin/DashboardCest.php
+++ tests/acceptance/admin/DashboardCest.php
@@ -1,27 +1,27 @@
 <?php
 
-namespace hipanel\modules\dashboard\tests\acceptance\client;
+namespace hipanel\modules\dashboard\tests\acceptance\admin;
 
-use hipanel\tests\_support\Step\Acceptance\Client;
+use hipanel\tests\_support\Step\Acceptance\Admin;
 use yii\helpers\Url;
 
 class DashboardCest
 {
-    public function ensureThatDashboardWorks(Client $I)
+    public function ensureThatDashboardWorks(Admin $I)
     {
         $I->login();
         $I->amOnPage(Url::to(['/']));
         $I->seeInCurrentUrl('/dashboard/dashboard');
         $I->see('Dashboard');
         $I->performOn('.content-wrapper', \Codeception\Util\ActionSequence::build()
+            ->see('Clients')
             ->see('Domains')
             ->see('Servers')
+            ->see('Parts')
+            ->see('Models')
             ->see('Tickets')
-            ->see('Finance')
-            ->dontSee('Clients')
             ->dontSee('Tariffs')
-            ->dontSee('Models')
-            ->dontSee('Parts')
+            ->dontSee('Finance')
         );
     }
 }
--- tests/acceptance/seller/DashboardCest.php
+++ tests/acceptance/seller/DashboardCest.php
@@ -1,27 +1,27 @@
 <?php
 
-namespace hipanel\modules\dashboard\tests\acceptance\client;
+namespace hipanel\modules\dashboard\tests\acceptance\seller;
 
-use hipanel\tests\_support\Step\Acceptance\Client;
+use hipanel\tests\_support\Step\Acceptance\Seller;
 use yii\helpers\Url;
 
 class DashboardCest
 {
-    public function ensureThatDashboardWorks(Client $I)
+    public function ensureThatDashboardWorks(Seller $I)
     {
         $I->login();
         $I->amOnPage(Url::to(['/']));
         $I->seeInCurrentUrl('/dashboard/dashboard');
         $I->see('Dashboard');
         $I->performOn('.content-wrapper', \Codeception\Util\ActionSequence::build()
+            ->see('Clients')
             ->see('Domains')
             ->see('Servers')
+            ->see('Parts')
+            ->see('Models')
             ->see('Tickets')
+            ->see('Tariffs')
             ->see('Finance')
-            ->dontSee('Clients')
-            ->dontSee('Tariffs')
-            ->dontSee('Models')
-            ->dontSee('Parts')
         );
     }
 }
